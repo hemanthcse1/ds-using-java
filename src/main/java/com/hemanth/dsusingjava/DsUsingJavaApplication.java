b66346abff37445e2b0761afbbe20140caf37384
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-09 19:53:37 +0530

    Initial commit

---
+++ src/main/java/com/hemanth/dsusingjava/DsUsingJavaApplication.java
@@ -0,0 +1,14 @@
+package com.hemanth.dsusingjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsUsingJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsUsingJavaApplication.class, args);
+        System.out.println("Data structures using Java");
+    }
+
+}
---
+++ src/test/java/com/hemanth/dsusingjava/DsUsingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.dsusingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsUsingJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
